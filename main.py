5ff5d352ab322040c372ded2f56ba8b2b7376454
Edmund Ochieng <edmund.ochieng@rackspace.com>
2018-09-19 18:18:53 -0500

    Built the template for the trivia on countries of the world

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask, jsonify, render_template
 import requests, json
 
-api_url = "https://restcountries.eu/rest/v2/name/{}?fields=name;capital"
+api_url = "https://restcountries.eu/rest/v2/name/{}?fields=name;capital;region"
 application = Flask(__name__)
 
+@application.route('/')
+def landing():
+  page = """<html><head><title>World Countries</title></head><body>
+    Welcome to worldcountries api. To interact with the api,
+    </body></html>"""
+  return page
+
+@application.route('/trivia')
+def world_countries():
+  url = 'http://127.0.0.1:8080/trivia'
+  response = requests.get(url)
+  if response.status_code == 200:
+    trivia_url = 'https://restcountries.eu/rest/v2/alpha/{}?fields=name;capital;region'.format(response.content)
+    trivia_response = requests.get(trivia_url)
+    if trivia_response.status_code == 200:
+      return  render_template('index.html', country=json.loads(str(trivia_response.content)))
+
 @application.route('/capital/<country>')
-def get_capital_city(country = None):
+def get_capital_of_country(country = None):
 	url = api_url.format(country)
 	response = requests.get(url)
 	if response.status_code == 200:
-		return response.content
+		return render_template('index.html', country=json.loads(str(trivia_response.content)))
 
 def main():
 	application.run(port=8090)
